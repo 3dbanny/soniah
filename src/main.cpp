6b19940ab36bc4299ec0ea6b95e69bbed31d8449
Dmytro <d.bannik@gmail.com>
2026-01-28 17:23:09 +0200

    major improvement of the code. Added new functions and structures

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,50 @@ GyverDBFile db(&LittleFS, "/data.db");
 SettingsGyver sett("SONIAH", &db);
 /*налаштування ключів БД*/
 enum kk : size_t {
-    wifi_ssid,
-    wifi_pass,
-    brightnessValue,
-    switchPosition1,
-    switchPosition2,
-    displayMode,
-    apply};
+  wifiSsid,
+  wifiPass,
+  brightnessValue,
+  switchPosition1,
+  switchPosition2,
+  displayMode,
+  apply
+};
+/*структура з глобальними змінними*/
+struct Data {
+  int batteryChargePercent = 0;
+};
+Data data;
+/*структура конфігурації розміру очей робота*/
+struct RoboEyesConfig {
+  const uint8_t EYE_WIDTH = 24;
+  const uint8_t EYE_HEIGHT = 24;
+  const uint8_t BORDER_RADIUS = 8;
+  const uint8_t SPACE_BETWEEN = 4;
+};
+
+  RoboEyesConfig roboeyesconfig;
+/*структура для управління живленням*/
+struct PowerManagement {
+  const float ADC_VOLTAGE_MULTIPLIER = 3.3 * 2.0 / 4095.0;
+  const float BATTERY_MAX_VOLTAGE = 4.2; // максимальна напруга батареї
+  const float BATTERY_MIN_VOLTAGE = 3.0; // мінімальна напруга батар
+  const int DIODE_DROP_MAH = 100; // падіння напруги на діодах в мА
+  const int BATTERY_CAPACITY_MAH = 2800; // ємність батареї в мАг
+  const int ESP32_CONSUMPTION_MAH = 80; // середнє споживання ESP32 в мА
+  const int MAX_LIGHT_CONSUMPTION_MAH = 200; // максимальне споживання ліхтаря в мА
+  const int BRIGHTNESS_MULTIPLIER = 255;
+};
+  PowerManagement powerManagement;
+
+/*структура для локалізації - ця частина навіть ще не почалася на виконання*/ 
+struct Lang {
+  // Вказуємо розмір [2], оскільки у нас 2 мови
+  const char* WIFI[2] = {"WiFi", "WiFi"};
+  const char* SSID[2] = {"ssid", "назва мережі"};
+  const char* PASSWORD[2] = {"password", "пароль"};
+};
+// Створюємо об'єкт структури, щоб до нього звертатися
+Lang lng;
 /*бібліотеки для роботи олед дисплея*/
 
 #include <Wire.h>
@@ -21,19 +58,18 @@ enum kk : size_t {
 #include <Adafruit_SSD1306.h>
 #include <FluxGarage_RoboEyes72x40.h>
 
-#define SDA_PIN 5
-#define SCL_PIN 6
+#define SdaPin 5
+#define SclPin 6
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define OLED_RESET    -1
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); 
+#define ScreenWidth 128
+#define ScreenHeight 64
+#define OledReset    -1
+Adafruit_SSD1306 display(ScreenWidth, ScreenHeight, &Wire, OledReset); 
 
 RoboEyes<Adafruit_SSD1306> roboEyes(display);
 
 /*=============бінд пінів=================*/
-#define voltmeterPin 5 // пін для вольтметра
-/*-------------------------------------------------------------------------------------*/
+#define voltmeterPin 7 // пін для вольтметра
 //піни для визначення позиції перемикача ліхтарика
 #define positionOnepin 1 //додати номер піна 
 #define positionTwopin 2 //додати номер піна 
@@ -42,15 +78,14 @@ RoboEyes<Adafruit_SSD1306> roboEyes(display);
 #define whiteLightPin 3 //додати номер піна
 //пін для керування яскравістю світла
 #define brightnessPin 0 //додати номер піна
-/*=====================змінні===========================================================*/
-String localIP;
-int percent = 0; // змінна для відсотків заряду батареї
+/*=======================================*/
 /*створення блоків веб  інтерфейсу*/
 void build(sets::Builder& b) {
+  b.Image(H(img), "", "/logo.png");
+  b.LinearGauge(H(batCharge), "Battery", 0, 100, "", data.batteryChargePercent,batteryWidgetColorChange(data.batteryChargePercent));
   if (b.beginGroup("WiFi")) {
-      b.Input(kk::wifi_ssid, "SSID");
-      b.Pass(kk::wifi_pass, "Password");
-      b.LabelNum(776,"Battery",percent);
+      b.Input(kk::wifiSsid, "SSID");
+      b.Pass(kk::wifiPass, "Password");
       if (b.Button(kk::apply, "Save & Restart")) {
       db.update();  // зберігаємо БД не очікуючи таймауту
       ESP.restart();
@@ -58,117 +93,152 @@ void build(sets::Builder& b) {
       b.endGroup(); 
   }
   if (b.beginGroup("Flashlight Settings")) {
-      //b.Spinner(kk::brightnessValue, "Brightness", 0, 255,1);
       b.Slider(kk::brightnessValue, "Brightness Slider", 0, 100,1);
-      b.Select(kk::switchPosition1, "Switch Position 1", "Red;White");
-      b.Select(kk::switchPosition2, "Switch Position 2", "Red;White");
-      b.Select(kk::displayMode, "Display Mode", "Percent;Time to discharge;Robot Eyes");
+      if (b.beginRow()) {
+        b.LED(H(led1), "Position 1",1, sets::Colors::Red,sets::Colors::Yellow);
+        b.Switch(kk::switchPosition1, "");
+        b.LED(H(led2), "",0, sets::Colors::Red,sets::Colors::Yellow);
+        b.endRow();
+      }
+
+      if (b.beginRow()) {
+        b.LED(H(led3), "Position 2",1, sets::Colors::Red,sets::Colors::Yellow);
+        b.Switch(kk::switchPosition2, "");
+        b.LED(H(led4), "",0, sets::Colors::Red,sets::Colors::Yellow);
+        b.endRow();
+      }
+
+      b.Select(kk::displayMode, "Display Mode", "Battery Charge;Time to discharge;Robot Eyes");
       b.endGroup();
   }
 }
-void update(sets::Updater u) {
-  u.update(776, percent);
 
+void update(sets::Updater u) {
+  u.update(H(batCharge), data.batteryChargePercent);
+  u.updateColor(H(batCharge), batteryWidgetColorChange(data.batteryChargePercent));
 }
 /*=========================ФУНКЦІЇ===========================*/
 /*визначення заряду батареї у відсотках*/
 int batCharge(uint8_t pin) {
-    int rawValue = analogRead(pin);  // Зчитуємо значення від 0 до 4095
-    float voltage = (rawValue * 1.48 / 1000);  // перетворюємо у напругу
-    float percentFloat = ((voltage - 3.2) * 100);
-    int percent = (int)percentFloat;
-    if (percent > 100) {
-        percent = 100;
-    } else if (percent < 0) {
-        percent = 0;
+  int rawValue = analogRead(pin);  // Зчитуємо значення від 0 до 4095
+  float voltage = rawValue * powerManagement.ADC_VOLTAGE_MULTIPLIER;  // перетворюємо у напругу
+
+  float percentFloat = ((voltage - powerManagement.BATTERY_MIN_VOLTAGE) / (powerManagement.BATTERY_MAX_VOLTAGE - powerManagement.BATTERY_MIN_VOLTAGE)) * 100.0;
+  int percent = (int)percentFloat;
+  if (percent > 100) {
+      percent = 100;
+  } else if (percent < 0) {
+      percent = 0;
     }  // Перетворюємо у відсотки
-    return percent;
+  return percent;
+}
+
+/*розрахунок кількості часу, що залишився до розрядки батареї*/
+int estimationTimeHours(int chargePercent,int brightnessLevel) {
+  int estimatedHours = 0;
+  int totalConsumptionMah = brightnessLevel * powerManagement.MAX_LIGHT_CONSUMPTION_MAH / 100 + powerManagement.ESP32_CONSUMPTION_MAH + powerManagement.DIODE_DROP_MAH; // розрахунок споживання ліхтаря в мА в залежності від яскравості
+  estimatedHours = (chargePercent * powerManagement.BATTERY_CAPACITY_MAH / 100 / totalConsumptionMah);  //приблизний розрахунок часу до розрядки батареї в годинах
+  return estimatedHours;
 }
 /*керування вибором кольору світла*/
 void bindPositionLight(int position) {
-
-    if (position == 0) {
-        digitalWrite(redLightPin, HIGH);
-        digitalWrite(whiteLightPin, LOW);
-    }
-    if (position == 1) {
-        digitalWrite(redLightPin, LOW);
-        digitalWrite(whiteLightPin, HIGH);
-    }
+  if (position == 0) {
+      digitalWrite(redLightPin, HIGH);
+      digitalWrite(whiteLightPin, LOW);
+  }
+  if (position == 1) {
+      digitalWrite(redLightPin, LOW);
+      digitalWrite(whiteLightPin, HIGH);
+  }
 }
 /*регулювання яскравості світла*/
 void adjustBrightness(int brightnessValue) {
-    analogWrite(brightnessPin, brightnessValue);
+  int pwmValue = brightnessValue * powerManagement.BRIGHTNESS_MULTIPLIER/100; // множник для переведення відсотків у значення від 0 до 255
+  analogWrite(brightnessPin, pwmValue);
 }
-/*розрахунок кількості часу, що залишився до розрядки батареї*/
-int estimationTimeHours(int chargePercent,int brightnessLevel) {
-    int estimatedHours = 0;
-    estimatedHours = (chargePercent * 2800 / 100 / (brightnessLevel+150));  //приблизний розрахунок часу до розрядки батареї в годинах
-    return estimatedHours;
+/*відображення кольору віджета батареї*/
+sets::Colors batteryWidgetColorChange(int value) {
+  if (value < 30) {
+      return sets::Colors::Red;
+  } else if (value < 70) {
+      return sets::Colors::Yellow;
+  } else {
+      return sets::Colors::Green;
+  }
 }
 /*відображення відсотків заряду батареї на OLED дисплеї*/
-void displayChargeLevelOled(int info) {
-    display.clearDisplay();
-    if (info == 100) {display.setCursor(30,10);}
-    else if (info >= 10) {display.setCursor(45,10);}
-    else {display.setCursor(54,10);}
-    display.println(info);
-    display.display();
+void displayChargeLevel(int info) {
+  static uint32_t tmrCharge = 0;
+  if (millis() - tmrCharge < 1000) return;  // оновлюємо не частіше 1 разу/сек
+  tmrCharge = millis();
+
+  display.clearDisplay();
+  if (info == 100) {
+      display.setCursor(30,10);
+  } else if (info >= 10) {
+      display.setCursor(45,10);
+  } else {
+      display.setCursor(54,10);
+  }
+  display.println(info);
+  display.display();
 }
 /*відображення кількості часу, що залишився до розрядки батареї на OLED дисплеї*/
-void displayestimationTime(int estimatedHours) {
-    display.clearDisplay();
-    if (estimatedHours >= 10) {display.setCursor(30,10);}
-    else {display.setCursor(45,10);}
-    //display.setCursor(30,10);//перший координат - по горизонталі, другий - по вертикалі 
-    display.println(String(estimatedHours) + "H");
-    display.display();
+void displayEstimationTime(int estimatedHours) {
+  static uint32_t tmrEstimation = 0;
+  if (millis() - tmrEstimation < 1000) return;  // оновлюємо не частіше 1 разу/сек
+  tmrEstimation = millis();
+    
+  display.clearDisplay();
+  if (estimatedHours >= 10) {
+      display.setCursor(30,10);
+  } else {display.setCursor(45,10);
+  }
+  //display.setCursor(30,10);//перший координат - по горизонталі, другий - по вертикалі 
+  display.println(String(estimatedHours) + "H");
+  display.display();
 }
 /*вибір рандомного числа*/
 int getRandomNumber(int maxValue) {
   return random(1, maxValue + 1);
 }
 /*анімація очей робота*/
-void robotSeeYou() {
-    roboEyes.update();
-  static uint32_t tmr2;
-  if (millis() - tmr2 >= 180000) { // кожні 3 хвилини змінюємо анімацію
-    int randNumFace = getRandomNumber(4);
-    tmr2 = millis();
-        switch (randNumFace) {
-            case 1:
-                roboEyes.setMood(DEFAULT);
-                roboEyes.anim_laugh();
-                break;
-            case 2:
-                roboEyes.setMood(TIRED);
-                roboEyes.anim_confused();
-                break;
-            case 3:
-                roboEyes.setMood(ANGRY);
-                roboEyes.anim_confused();
-                break;
-            case 4:
-                roboEyes.setMood(HAPPY);
-                roboEyes.anim_laugh();
-                break;
-        }
+void displayRoboEyesAnimation() {
+  roboEyes.update();
+  static uint32_t tmrAnimation;
+  const unsigned long EYE_ANIMATION_INTERVAL = 3 * 60 * 1000; // інтервал зміни анімації в мілісекундах (3 хвилини)
+  if (millis() - tmrAnimation >= EYE_ANIMATION_INTERVAL) { // кожні 3 хвилини змінюємо анімацію
+      int randNumFace = getRandomNumber(4);
+      tmrAnimation = millis();
+      switch (randNumFace) {
+          case 1:
+              roboEyes.setMood(DEFAULT);
+              roboEyes.anim_laugh();
+              break;
+          case 2:
+              roboEyes.setMood(TIRED);
+              roboEyes.anim_confused();
+              break;
+          case 3:
+              roboEyes.setMood(ANGRY);
+              roboEyes.anim_confused();
+              break;
+          case 4:
+              roboEyes.setMood(HAPPY);
+              roboEyes.anim_laugh();
+              break;
+      }
   }
 }
 /*мапінг режимів перемикача та кольорів ліхтаря*/
 void manageSwitcherPosition() {
   if (digitalRead(positionOnepin) == digitalRead(positionTwopin)) {
       bindPositionLight(db[kk::switchPosition2]);
-  }
-  else if (digitalRead(positionOnepin) != digitalRead(positionTwopin)) {
+  } else if (digitalRead(positionOnepin) != digitalRead(positionTwopin)) {
       bindPositionLight(db[kk::switchPosition1]);
-  }
-  
+  } 
 }
 /*----------------------------------------------------------------------------*/
-
-
-
 void setup() {
   Serial.begin(115200); // 115200 baud rate
   pinMode(voltmeterPin, INPUT);
@@ -177,45 +247,42 @@ void setup() {
   pinMode(redLightPin, OUTPUT);
   pinMode(whiteLightPin, OUTPUT);
 
-  percent = batCharge(voltmeterPin);// перший раз отримуэмо значення заряду батареї. Наступний раз буде через 5 хвилин
+  data.batteryChargePercent = batCharge(voltmeterPin);// перший раз отримуэмо значення заряду батареї. Наступний раз буде через 5 хвилин
   // ======== WIFI ========
-
   // STA
   WiFi.mode(WIFI_AP_STA);
-// ======== SETTINGS ========
-  sett.begin(); // базу даних підключаємо до підключення до точки
+  // ======== SETTINGS ========
+  sett.begin(true,"soniah"); // базу даних підключаємо до підключення до точки
   sett.onBuild(build);
   sett.onUpdate(update);
   // ======== DATABASE ========
-#ifdef ESP32
+  #ifdef ESP32
     LittleFS.begin(true);
-#else
+  #else
     LittleFS.begin();
-#endif
+  #endif
 
   db.begin();
 
   // ініціювання БД початковими даними
-    db.init(kk::wifi_ssid, "");
-    db.init(kk::wifi_pass, "");
-    db.init(kk::brightnessValue, 128);
+    db.init(kk::wifiSsid, "");
+    db.init(kk::wifiPass, "");
+    db.init(kk::brightnessValue, 100);
     db.init(kk::switchPosition1, 0);
     db.init(kk::switchPosition2, 1);
     db.init(kk::displayMode, 2);
 
-  
-
-// ======= AP =======
-  WiFi.softAP("SONIAH");
+  // ======= AP =======
+  WiFi.softAP("SONIAH_" + String(random((100))));
   Serial.print("AP IP: ");
   Serial.println(WiFi.softAPIP());
 
   // ======= STA =======
   // если логин задан - подключаемся
-  if (db[kk::wifi_ssid].length()) {
-    WiFi.begin(db[kk::wifi_ssid], db[kk::wifi_pass]);
+  if (db[kk::wifiSsid].length()) {
+    WiFi.begin(db[kk::wifiSsid], db[kk::wifiPass]);
     Serial.print("Connect STA");
-    int tries = 20;
+    int tries = 5;
     while (WiFi.status() != WL_CONNECTED) {
       delay(500);
       Serial.print('.');
@@ -224,13 +291,13 @@ void setup() {
       Serial.println();
       Serial.print("IP: ");
       Serial.println(WiFi.localIP());
-      //localIP = WiFi.localIP().toString();
+      
   }
 
 
 
   /*=======================SETUP OLED========================================*/
-  Wire.begin(SDA_PIN, SCL_PIN);
+  Wire.begin(SdaPin, SclPin);
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
@@ -240,17 +307,19 @@ void setup() {
   display.setTextSize(3);
   display.setTextColor(SSD1306_WHITE);
   /*=======================SETUP robo eyes========================================*/
-  roboEyes.begin(SCREEN_WIDTH, SCREEN_HEIGHT, 100);
+  roboEyes.begin(ScreenWidth, ScreenHeight, 100);
   // Define some automated eyes behaviour
   roboEyes.setAutoblinker(ON, 3, 2); // Start auto blinker animation cycle -> bool active, int interval, int variation -> turn on/off, set interval between each blink in full seconds, set range for random interval variation in full seconds
   // Define eye shapes, all values in pixels
-  roboEyes.setWidth(24, 24); // byte leftEye, byte rightEye
-  roboEyes.setHeight(24, 24); // byte leftEye, byte rightEye
-  roboEyes.setBorderradius(8, 8); // byte leftEye, byte rightEye
-  roboEyes.setSpacebetween(4); // int space -> can also be neg
+  
+
+  roboEyes.setWidth(roboeyesconfig.EYE_WIDTH, roboeyesconfig.EYE_WIDTH);
+  roboEyes.setHeight(roboeyesconfig.EYE_HEIGHT, roboeyesconfig.EYE_HEIGHT);
+  roboEyes.setBorderradius(roboeyesconfig.BORDER_RADIUS, roboeyesconfig.BORDER_RADIUS);
+  roboEyes.setSpacebetween(roboeyesconfig.SPACE_BETWEEN);
   //roboEyes.setHFlicker(ON, 2); // horizontal flickering effect -> bool active, int intensity (1-5)
   roboEyes.setPosition(NE); // cardinal directions, can be N, NE, E, SE, S, SW, W, NW, DEFAULT (default = horizontally and vertically centered)
-
+  //вітальна фраза на олед дисплеї
   display.clearDisplay();
   display.setTextSize(3);
   display.setTextColor(SSD1306_WHITE);
@@ -262,62 +331,31 @@ void setup() {
 
 
 void loop() {
-  Serial.begin(115200);
-  
+  static uint32_t tmrBattery;
+  const unsigned long BATTERY_CHARGE_INTERVAL = 5 * 60 * 1000; // інтервал зміни анімації в мілісекундах (5 хвилин)
+  if (millis() - tmrBattery >= BATTERY_CHARGE_INTERVAL) { // кожні 5 хвилин змінюємо анімацію
+  data.batteryChargePercent = batCharge(voltmeterPin);
+  tmrBattery = millis();
+  }
   /*======================switcher position manager===================*/
   manageSwitcherPosition();
   /*========================BRIGHTNESS=================================*/
-  adjustBrightness(db[kk::brightnessValue*25]);
+  adjustBrightness(db[kk::brightnessValue]);
 
   /*========================robot eyes=================================*/
   switch ((int)db[kk::displayMode]){
     /*======================== display percent of battery charge=================================*/
     case 0:
-      display.clearDisplay();
-      display.setCursor(30,10);
-      display.println(100);
-      display.display();
+      displayChargeLevel(data.batteryChargePercent);
       break;
     /*========================display quantity hours to battery discharge=================================*/
     case 1:
-      display.clearDisplay();
-      display.setCursor(30,10);
-      display.println("10H");
-      display.display();
+      displayEstimationTime(estimationTimeHours(data.batteryChargePercent,db[kk::brightnessValue]));
       break;
     case 2:
-      robotSeeYou();
+      displayRoboEyesAnimation();
       break;
   }
-  /*depricated*/
-  // if (db[kk::displayMode] == 2) {
-  //   robotSeeYou();
-  // };
-  // if (db[kk::displayMode] == 1) {
-  //   display.clearDisplay();
-  //   display.setCursor(30,10);
-  //   display.println("10H");
-  //   display.display();
-  //   /*static uint32_t tmr2;
-  //   if (millis() - tmr2 >= 300000) { // кожні 5 хвилин оновлюэмо значення кількості часу до розрядки батареї та оновлюємо відображення на олед дисплеї
-  //     displayestimationTime(estimationTimeHours(batCharge(voltmeterPin),db[kk::brightnessValue*25]));
-  //     tmr2 = millis();  
-  //   }; */
-  // };
-   
-  // /*======================== display percent of battery charge=================================*/
-  // if (db[kk::displayMode] == 0) {
-  //   display.clearDisplay();
-  //   display.setCursor(30,10);
-  //   display.println(100);
-  //   display.display();
-  //   /*static uint32_t tmr3;
-  //   if (millis() - tmr3 >= 300000) { // кожні 5 хвилин оновлюємо значення заряду батареї та оновлюємо відображення на олед дисплеї
-  //     displayChargeLevelOled(batCharge(voltmeterPin));
-  //     tmr3 = millis();  
-  //   }; */
-  // };
-  int temperature = temperatureRead();
   sett.tick();
 }
 
